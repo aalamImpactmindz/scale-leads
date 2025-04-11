7ecabc50f45e0c2c79c0b2ad9e4a292b3e31c369
aalamImpactmindz <aalam.impactmindz@gmail.com>
2025-04-11 18:43:06 +0530

    Added success stories section.

--- app/page.js
+++ app/page.js
@@ -3,15 +3,22 @@ import Hero from "@/components/hero/Hero";
 import Working from "@/components/working/Working";
 import Plans from "@/components/plans/Plans";
 import Faqs from "@/components/faqs/Faqs";
+import SuccessStories from "@/components/success-stories/SuccessStories";
+import { Button } from "react-bootstrap";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="page-home">
+      <Link href="/abonnement" passHref className="position-fixed">
+        <Button className="btn-main w-100">Démarrer</Button>
+      </Link>
       <Hero />
       <ContinuousSlider />
       <Working />
       <Plans />
+      <SuccessStories />
       <Faqs />
-    </>
+    </div>
   );
 }
---
+++ components/success-stories/SuccessStories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Heading from "../heading/Heading";
+
+const SuccessStories = () => {
+  return (
+    <section className="success-stories sec-padding">
+      <Container>
+        <Heading
+          title="Customer Success Stories"
+          highlightedWords={["Success"]}
+          description="Discover how our platform has helped businesses create outstanding content effortlessly. Hear directly from our users about their success and satisfaction."
+        />
+      </Container>
+    </section>
+  );
+};
+
+export default SuccessStories;
