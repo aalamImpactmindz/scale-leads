5f0f9c6a60e1c5617235c456afdd1cf15a80eb47
aalamImpactmindz <aalam.impactmindz@gmail.com>
2025-04-28 18:44:18 +0530

    Registration form.

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -22,6 +22,16 @@ const Register = () => {
     name: "",
     email: "",
     password: "",
+    ideal_customer: "",
+    sector: "",
+    company_size: "",
+    objective: "",
+    offer: "",
+    website: "",
+    channel: "",
+    tone: "",
+    existing_messages: "",
+    competitors: "",
   });
 
   const [message, setMessage] = useState("");
@@ -55,6 +65,16 @@ const Register = () => {
           name: "",
           email: "",
           password: "",
+          ideal_customer: "",
+          sector: "",
+          company_size: "",
+          objective: "",
+          offer: "",
+          website: "",
+          channel: "",
+          tone: "",
+          existing_messages: "",
+          competitors: "",
         });
         setConfirmPassword("");
       } else {
@@ -70,12 +90,14 @@ const Register = () => {
     <div className="page-register sec-padding">
       <Container fluid="xl">
         <Heading title="My Account" />
-        <Row className="row-cols-1 row-cols-md-2 g-3 g-md-4">
-          <Col>
-            <div className="bg-gray p-3 p-lg-4">
-              <h4 className="color-light">Register</h4>
-              <p>Start your journey with us—it's quick and simple.</p>
-              <Form className="mt-4" onSubmit={handleSubmit}>
+        {/* <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+          <Col> */}
+        <div className="bg-gray p-3 p-lg-4">
+          <h4 className="color-light">Register</h4>
+          <p>Start your journey with us—it's quick and simple.</p>
+          <Form className="mt-4" onSubmit={handleSubmit}>
+            <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+              <Col>
                 <Form.Group className="mb-3" controlId="formRegisterFullName">
                   <Form.Label>Full Name</Form.Label>
                   <Form.Control
@@ -88,6 +110,8 @@ const Register = () => {
                     }
                   />
                 </Form.Group>
+              </Col>
+              <Col>
                 <Form.Group className="mb-3" controlId="formRegisterEmail">
                   <Form.Label>Email address</Form.Label>
                   <Form.Control
@@ -100,7 +124,10 @@ const Register = () => {
                     }
                   />
                 </Form.Group>
-
+              </Col>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+              <Col>
                 <Form.Group className="mb-3" controlId="formRegisterPassword">
                   <Form.Label>Password</Form.Label>
                   <Form.Control
@@ -113,6 +140,8 @@ const Register = () => {
                     }
                   />
                 </Form.Group>
+              </Col>
+              <Col>
                 <Form.Group
                   className="mb-3"
                   controlId="formRegisterConfirmPassword"
@@ -126,26 +155,192 @@ const Register = () => {
                     onChange={(e) => setConfirmPassword(e.target.value)}
                   />
                 </Form.Group>
-                <Button className="btn-main" type="submit">
-                  Register
-                </Button>
-                {message && (
-                  <Alert
-                    variant="success"
-                    className="mt-3 small py-2 rounded-0"
-                  >
-                    {message}
-                  </Alert>
-                )}
-                {error && (
-                  <Alert variant="danger" className="mt-3 small py-2 rounded-0">
-                    {error}
-                  </Alert>
-                )}
-              </Form>
-            </div>
-          </Col>
-          <Col>
+              </Col>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+              <Col>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formRegisterIdealCustomer"
+                >
+                  <Form.Label>Who's your ideal customer?</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Customer"
+                    required
+                    value={formData.ideal_customer}
+                    onChange={(e) =>
+                      setFormData({ ...formData, ideal_customer: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3" controlId="formRegisterSector">
+                  <Form.Label>What sector does he work in?</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Sector Name"
+                    required
+                    value={formData.sector}
+                    onChange={(e) =>
+                      setFormData({ ...formData, sector: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+              <Col>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formRegisterCompanySize"
+                >
+                  <Form.Label>What size company are you targeting?</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Size"
+                    required
+                    value={formData.company_size}
+                    onChange={(e) =>
+                      setFormData({ ...formData, company_size: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3" controlId="formRegisterObjective">
+                  <Form.Label>What is your main objective?</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Your Objective"
+                    required
+                    value={formData.objective}
+                    onChange={(e) =>
+                      setFormData({ ...formData, objective: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+              <Col>
+                <Form.Group className="mb-3" controlId="formRegisterYourOffer">
+                  <Form.Label>What's your offer (your promise)?</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Your Offer"
+                    required
+                    value={formData.offer}
+                    onChange={(e) =>
+                      setFormData({ ...formData, offer: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formRegisterYourWebsite"
+                >
+                  <Form.Label>Do you have a website?</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Website"
+                    required
+                    value={formData.website}
+                    onChange={(e) =>
+                      setFormData({ ...formData, website: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+              <Col>
+                <Form.Group className="mb-3" controlId="formRegisterChannel">
+                  <Form.Label>
+                    Which channel do you want to prospect on?
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Channel"
+                    required
+                    value={formData.channel}
+                    onChange={(e) =>
+                      setFormData({ ...formData, channel: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3" controlId="formRegisterTone">
+                  <Form.Label>
+                    What tone do you want your messages to take?
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="(optional)"
+                    value={formData.tone}
+                    onChange={(e) =>
+                      setFormData({ ...formData, tone: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-2 g-0 g-md-4">
+              <Col>
+                <Form.Group className="mb-3" controlId="formRegisterMessages">
+                  <Form.Label>
+                    Do you already have messages you've used?
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="(optional)"
+                    value={formData.existing_messages}
+                    onChange={(e) =>
+                      setFormData({ ...formData, existing_messages: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formRegisterCompetitors"
+                >
+                  <Form.Label>
+                    Any competitors or approaches you'd like to avoid?
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="(optional)"
+                    value={formData.competitors}
+                    onChange={(e) =>
+                      setFormData({ ...formData, competitors: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button className="btn-main" type="submit">
+              Register
+            </Button>
+            {message && (
+              <Alert variant="success" className="mt-3 small py-2 rounded-0">
+                {message}
+              </Alert>
+            )}
+            {error && (
+              <Alert variant="danger" className="mt-3 small py-2 rounded-0">
+                {error}
+              </Alert>
+            )}
+          </Form>
+        </div>
+        {/* </Col> */}
+        {/* <Col>
             <div className="bg-gray p-3 p-lg-4 h-100">
               <h4 className="color-light">Already Have an Account?</h4>
               <p>
@@ -156,8 +351,8 @@ const Register = () => {
                 <Button className="btn-main">Login</Button>
               </Link>
             </div>
-          </Col>
-        </Row>
+          </Col> */}
+        {/* </Row> */}
       </Container>
     </div>
   );
