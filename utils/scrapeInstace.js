53aad6b6e5a034fb179facaf184fb770c5e2855e
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-06-20 11:33:20 +0530

    update

--- utils/scrapeInstace.js
+++ utils/scrapeInstace.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 const scrapInstance = axios.create({
-   baseURL: "https://linkdin-kty8.onrender.com/"
+   baseURL: "https://linkdin-bnyj.onrender.com/"
  // baseURL: "http://localhost:4000/"
 });
 
