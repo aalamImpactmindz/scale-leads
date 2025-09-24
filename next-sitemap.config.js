ee434d7ced74dc52d2378099fab3958922175dac
sanjayimpact <sanjay.impactmindz@gmail.com>
2025-09-24 17:26:27 +0530

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://scaleleads.fr',
+  generateRobotsTxt: true,
+  changefreq: 'daily',        // tells Google how often pages update
+  priority: 0.8,              // relative importance (0.0 - 1.0)
+  sitemapSize: 7000,
+  exclude: ['/api/*'],        // exclude API routes if not needed
+};
